1f5fb51393990a7c224f1145d7e0f892588530c7
Patrick Johnstone <johnstonep@gmail.com>
2017-07-26 13:00:41 +0100

    Proper init

---
+++ patrick/CountryCurrency/CountryCurrency/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CountryCurrency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string[]> countries = new List<string[]>(); 
+            string[] lines = File.ReadAllLines(@"\\nasbox\development\content.txt");
+            foreach (string line in lines)
+            {
+                string[] values = line.Split(',');
+                countries.Add(values);
+            }
+            Console.WriteLine("Enter a Country");
+            string countryin = Console.ReadLine();
+            string currency;
+            foreach (string[] country in countries)
+            {
+                if (country[0] == countryin)
+                {
+                    currency = country[2];
+                    break;
+                }
+            }
+        }   
+    }
+}
